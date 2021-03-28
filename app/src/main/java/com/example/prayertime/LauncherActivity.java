9a4ee06db872a750235b753e5aacef0f66d886ca
ha4lool <dalalbinhumaid@gmail.com>
2021-03-29 00:18:45 +0300

    Added them and style configurations

---
+++ app/src/main/java/com/example/prayertime/LauncherActivity.java
@@ -0,0 +1,27 @@
+package com.example.prayertime;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LauncherActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(LauncherActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+    }
+}
