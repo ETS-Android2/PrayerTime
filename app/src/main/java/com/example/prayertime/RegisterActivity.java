1904c22554b5e752417591c18a4054627bd2d7d0
Haifa Hamad <haifahamad1420@gmail.com>
2021-04-01 12:51:49 +0300

    .

--- app/src/main/java/com/example/prayertime/RegisterActivity.java
+++ app/src/main/java/com/example/prayertime/RegisterActivity.java
@@ -3,9 +3,15 @@ package com.example.prayertime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     DatabaseHelper myDb;
+    EditText email, password, rePassword;
+    Button register;
+    TextView haveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
